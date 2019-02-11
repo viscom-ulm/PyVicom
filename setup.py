624b6ffd854542a4962690d5b6be830337c43942
sebastian <sebastian.hartwig@uni-ulm.de>
2019-02-11 10:43:06 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import setuptools
 
 setuptools.setup(
-    name='ViscomFramework',
+    name='PyViscom',
     version='0.0.1',
-    description='collection of usefull stuff',
+    description='Python framework used for exercises of the visual computing group at the Ulm University.',
     url='',
     author='Sebastian Hartwig',
     author_email='sebastian.hartwig@uni-ulm.de',
     maintainer='Sebastian Hartwig',
     maintainer_email='sebastian.hartwig@uni-ulm.de',
     packages=setuptools.find_packages(),
-    install_requires=['numpy', 'pyrr', 'glfw', 'PyOpenGL'],
+    install_requires=['numpy', 'glfw', 'PyOpenGL'],
     include_package_data=True
 )
